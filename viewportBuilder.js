2b60a89912f3a8ab42cae32c121d2a74cd956d1a
Tim Davis <thebearingedge@gmail.com>
2015-07-14 21:36:14 -0700

    typo

--- viewportBuilder.js
+++ viewportBuilder.js
@@ -4,7 +4,7 @@
 /*
  * user-provided state objects come with view keys that refer to viewports.
  * many "views" populate the same viewports at different states. we may want
- * the view to hold references to its viewports. one ocurrence of an implied
+ * the state to hold references to its viewports. one ocurrence of an implied
  * viewport is all we need to a) instantiate the viewport and b) add it to the
  * implied state. a given state object may imply a viewport that exists on a yet
  * unaccounted-for state, so we will queue info about the viewport until we can

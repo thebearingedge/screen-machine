1abf58ee09fbc86e1fc6f9e019b03c7fae00bff2
Tim Davis <thebearingedge@gmail.com>
2015-08-14 21:33:23 -0700

    nothing

--- screenMachine.js
+++ screenMachine.js
@@ -15,13 +15,15 @@ module.exports = screenMachine;
 function screenMachine(config) {
 
   var document = config.document;
+  var window = document.defaultView;
   var Promise = config.promises;
   var Component = config.components(document);
   var views = viewTree(document, Component);
   var resolves = resolveService(Promise);
-  var routes = router(document.defaultView, { html5: config.html5 });
+  var routes = router(window, { html5: config.html5 });
   var states = stateRegistry(views, resolves, routes);
   var events = eventBus(config.events);
+
   var machine = stateMachine(events, states, resolves, routes, views);
 
   return machine.init(states.$root, {});
